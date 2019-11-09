ec1e216e5541b1285030e5a328ca5c0afd9778f1
alinisar <alinisar2002@gmail.com>
2019-11-08 20:27:05 -0500

    Add files via upload

---
+++ bottles of beer project.py
@@ -0,0 +1,10 @@
+#Bottles of Beer on the Wall Project by Ali Nisar
+
+count = 99
+
+while count > 0:
+    print(str(count) + ' bottles of beer on the wall')
+    count = count - 1
+else:
+    print('There are now 0 bottles of beer on the wall. Congratulations! You drank it all asshole!')
+        
